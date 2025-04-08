0c85d1cec32d54e9f7bdab16f96eb15df039ae2e
ziraqi <126034358+ziraqi@users.noreply.github.com>
2025-04-08 09:41:42 -0600

    Resolved issue #21, Adding a description of the Number Guessing Game (#61)
    * added a brief description when selecting game

--- NumberGuessGame.java
+++ NumberGuessGame.java
@@ -13,7 +13,11 @@ import java.util.Optional;
 class NumberGuessGame implements Game {
     public String getName() { return "Number Guess"; }
     public Optional<Integer> play() {
-        System.out.println("[Playing Number Guess - Placeholder]");
+        System.out.println("[Playing Number Guessing Game]");
+        System.out.println("This is a simple game where you"
+                           + "try and guess the number I pick.");
+        System.out.println("If you are too high or too low I will let"
+                           + "you know. Try to do it in the fewest attempts!");
         return Optional.empty();
     }
 }
