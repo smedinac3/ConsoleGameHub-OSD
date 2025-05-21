f2c73aa026bde94538384c19ca113aee293199f4
Jody <jody@acm.org>
2025-05-20 23:24:11 -0600

    Addressed additional identified code quality issues.

--- GameUtils.java
+++ GameUtils.java
@@ -1,31 +1,32 @@
 import java.util.Random;
 import java.util.List;
 /**
-*This class provides utility methods for handling random word selection.
-*It contains a method to get a random word from a list of words.
-*/
+ * This utility class provides methods for use in multiple games.
+ * For example, it contains a method for handling random word selection.
+ */
 public final class GameUtils {
     /**
-    *Prevent Instantiation.
-    */
+     * Static instance used to generate random strings.
+     */
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Prevent Instantiation.
+     */
     private GameUtils() {
         throw new UnsupportedOperationException("class cannot be instantiated");
     }
+
     /**
-    *Static instance used to generate random strings.
-    */
-    private static final Random RANDOM = new Random();
-    /**
-    *Returns a random word from given list.
-    *
-    *@param words A list of words from which the random word will be selected.
-    *@return A random word from the list, or null if the list is null or empty.
-    */
+     * Returns a random word from given list.
+     *
+     * @param words A list of words from which the random word will be selected.
+     * @return A random word from the list or null if the list is null or empty.
+     */
     public static String getRandomWord(final List<String> words) {
         if (words == null || words.isEmpty()) {
             return null;
             }
-        int randomIndex = RANDOM.nextInt(words.size());
-        return words.get(randomIndex);
+        return words.get(RANDOM.nextInt(words.size()));
      }
 }
